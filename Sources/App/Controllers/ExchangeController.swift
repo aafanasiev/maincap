356bf26e030723da7cbf484f2115d493b7ef93bc
Afanasiev Aleksandr <afanasiev.aleksandr.92@gmail.com>
2018-12-31 13:00:15 +0200

    Hot fix

--- Sources/App/Controllers/ExchangeController.swift
+++ Sources/App/Controllers/ExchangeController.swift
@@ -99,7 +99,7 @@ final class ExchangeController {
     func ticker(_ req: Request) throws -> String  {
         
         let group = DispatchGroup()
-        var tickerArray = [[String : AnyObject]]()
+        let tickerArray = [[String : AnyObject]]()
         
    
         var tickers = [String : String]()
@@ -114,16 +114,16 @@ final class ExchangeController {
         }
         group.wait()
         
-                getBinanceTickers { tickers in
-        
-                    if let tic = tickers {
-                        tickerArray = tic
-                        group.leave()
-                    }
+//                getBinanceTickers { tickers in
+//
+//                    if let tic = tickers {
+//                        tickerArray = tic
+//                        group.leave()
+//                    }
         
 //                                tickerArray = tickers!
         
-                }
+//                }
         print(tickerArray)
 
         
@@ -260,31 +260,31 @@ final class ExchangeController {
     
     
 //    func getBinanceTickers(completion: @escaping([[String : AnyObject]]?) -> Void) {
-//        
+//
 //        var tickers = [[String:AnyObject]]()
-//        
+//
 //        let session = URLSession.shared
 //        let url = URL(string: "https://api.binance.com/api/v1/ticker/24hr")
-//        
+//
 //        var request = URLRequest(url: url!)
 //        request.httpMethod = "GET"
-//        
+//
 //        session.dataTask(with: request) { (data, resp, error) in
-//            
+//
 //            if error != nil {
 //                completion(nil)
 //            }
-//            
+//
 //            if let dat = data {
-//                
+//
 //                do {
-//                    
+//
 //                    var usdPrice: Double = 0
 //                    var yesterdayPrice: Double = 0
-//                    
+//
 //                    guard let array = try JSONSerialization.jsonObject(with: dat, options: []) as? [[String : Any]] else {return}
-//                    
-//                    
+//
+//
 //                    _ = array.filter({ dict -> Bool in
 //                        if let symbol = dict["symbol"] as? String {
 //                            if symbol == "BTCUSDT" {
@@ -293,87 +293,87 @@ final class ExchangeController {
 //                        }
 //                        return false
 //                    }).map({ dict -> Void in
-//                        
+//
 //                        if let price = dict["weightedAvgPrice"] as? String, let change = dict["priceChange"] as? String {
-//                            
+//
 //                            usdPrice = Double(price)!
 //                            yesterdayPrice = Double(price)! - Double(change)!
-//                            
+//
 //                        }
-//                        
+//
 //                    })
-//                    
+//
 //                    _ = array.map({ dict -> Void in
 //                        if let symbol = dict["symbol"] as? String, let price = dict["weightedAvgPrice"] as? String, let change = dict["priceChangePercent"] as? String, let changeValue = dict["priceChange"] as? String {
 //                            if symbol.suffix(3) == "BTC" && Double(price)! != 0 {
 //                                //                                print("\(symbol) - \(price) - \(change)")
-//                                
+//
 //                                let sym = symbol.dropLast(3).description
 //                                let tickerUSDPrice = (Double(price)! * usdPrice)
 //                                print(sym)
-//                                
+//
 //                                let assetYesterdayPice = Double(price)! - Double(changeValue)!
-//                                
+//
 //                                let tickerYesterdayPrice = yesterdayPrice * assetYesterdayPice
-//                                
+//
 //                                let changeUSD = (tickerUSDPrice - tickerYesterdayPrice) / tickerYesterdayPrice
-//                                
+//
 //                                let changeBTC = NSDecimalNumber(string: change).dividing(by: NSDecimalNumber(value: 100))
-//                                
+//
 //                                let ticker = ["symbol" : sym,
 //                                              "priceBTC" : price,
 //                                              "changeBTC" : changeBTC.stringValue,
 //                                              "priceUSDT" : tickerUSDPrice.description,
 //                                              "changeUSDT" : changeUSD.description]
-//                                
-//                                
+//
+//
 //                                //                                    Ticker(symbol: sym, priceUSDT: tickerUSDPrice.description, priceBTC: price, changeUSDT: changeUSD.description, changeBTC: changeBTC.stringValue)
 //                                tickers.append(ticker as [String : AnyObject])
-//                                
+//
 //                            }
 //                        }
 //                    })
-//                    
+//
 //                    completion(tickers)
-//                    
+//
 //                } catch {
 //                    print("Error: cannot create JSON from todo")
 //                    return
 //                }
-//                
-//                
+//
+//
 //            }
-//            
+//
 //            }.resume()
 //    }
-//    
+//
 //    func getBittrexTickers(completion: @escaping([[String : AnyObject]]?) -> Void) {
-//        
+//
 //        var tickers = [[String:AnyObject]]()
-//        
+//
 //        let session = URLSession.shared
 //        let url = URL(string: "https://bittrex.com/api/v1.1/public/getmarketsummaries")
-//        
+//
 //        var request = URLRequest(url: url!)
 //        request.httpMethod = "GET"
-//        
+//
 //        session.dataTask(with: request) { (data, resp, error) in
-//            
+//
 //            if error != nil {
 //                completion(nil)
 //            }
-//            
+//
 //            if let dat = data {
-//                
+//
 //                do {
-//                    
+//
 //                    var usdPrice: Double = 0
 //                    var yesterdayPrice: Double = 0
-//                    
+//
 //                    guard let array = try JSONSerialization.jsonObject(with: dat, options: []) as? [String : Any], let result = array["result"] as? [[String : AnyObject]] else {return}
-//                    
-//                    
-//                    
+//
+//
+//
 //                    _ = result.filter({ dict -> Bool in
 //                        if let symbol = dict["MarketName"] as? String {
 //                            if symbol == "USDT-BTC" {
@@ -382,94 +382,94 @@ final class ExchangeController {
 //                        }
 //                        return false
 //                    }).map({ dict -> Void in
-//                        
+//
 //                        if let price = dict["Last"] as? Double, let prevDay = dict["PrevDay"] as? Double {
-//                            
-//                            
+//
+//
 //                            usdPrice = price
 //                            yesterdayPrice = prevDay
-//                            
+//
 //                        }
-//                        
+//
 //                    })
-//                    
+//
 //                    _ = result.map({ dict -> Void in
 //                        if let symbol = dict["MarketName"] as? String, let price = dict["Last"] as? Double, let prevDay = dict["PrevDay"] as? Double {
-//                            
+//
 //                            if symbol.prefix(4) == "BTC-" {
 //                                //                                print("\(symbol) - \(price) - \(change)")
-//                                
+//
 //                                let sym = symbol.dropFirst(4).description
-//                                
+//
 //                                let tickerUSDPrice = price * usdPrice
-//                                
+//
 //                                let changeBTC = (price - prevDay) / prevDay
-//                                
+//
 //                                let priceBTC = NSDecimalNumber(value: price)
-//                                
-//                                
+//
+//
 //                                let assetYesterdayPice = yesterdayPrice * price * prevDay
-//                                
+//
 //                                //                                let tickerYesterdayPrice = yesterdayPrice * assetYesterdayPice
-//                                
+//
 //                                let changeUSD = (tickerUSDPrice - assetYesterdayPice) / assetYesterdayPice
-//                                
+//
 //                                //                                let changeBTC = NSDecimalNumber(string: change).dividing(by: NSDecimalNumber(value: 100))
-//                                
+//
 //                                let ticker = ["symbol" : sym,
 //                                              "priceBTC" : priceBTC.stringValue,
 //                                              "changeBTC" : changeBTC.description,
 //                                              "priceUSDT" : tickerUSDPrice.description,
 //                                              "changeUSDT" : changeUSD.description]
-//                                
-//                                
+//
+//
 //                                //                                    Ticker(symbol: sym, priceUSDT: tickerUSDPrice.description, priceBTC: price, changeUSDT: changeUSD.description, changeBTC: changeBTC.stringValue)
 //                                tickers.append(ticker as [String : AnyObject])
-//                                
+//
 //                            }
 //                        }
 //                    })
-//                    
-//                    
+//
+//
 //                    completion(tickers)
-//                    
+//
 //                } catch {
 //                    print("Error: cannot create JSON from todo")
 //                    return
 //                }
-//                
-//                
+//
+//
 //            }
-//            
+//
 //            }.resume()
 //    }
-//    
-//    
+//
+//
 //    func getHitBTCTickers(completion: @escaping([[String : AnyObject]]?) -> Void) {
-//        
+//
 //        var tickers = [[String:AnyObject]]()
-//        
+//
 //        let session = URLSession.shared
 //        let url = URL(string: "https://api.hitbtc.com/api/2/public/ticker")
-//        
+//
 //        var request = URLRequest(url: url!)
 //        request.httpMethod = "GET"
-//        
+//
 //        session.dataTask(with: request) { (data, resp, error) in
-//            
+//
 //            if error != nil {
 //                completion(nil)
 //            }
-//            
+//
 //            if let dat = data {
-//                
+//
 //                do {
-//                    
+//
 //                    var usdPrice: Double = 0
 //                    var yesterdayPrice: Double = 0
-//                    
+//
 //                    guard let array = try JSONSerialization.jsonObject(with: dat, options: []) as? [[String : AnyObject]] else {return}
-//                    
+//
 //                    _ = array.filter({ dict -> Bool in
 //                        if let symbol = dict["symbol"] as? String {
 //                            if symbol == "BTCUSD" {
@@ -478,67 +478,67 @@ final class ExchangeController {
 //                        }
 //                        return false
 //                    }).map({ dict -> Void in
-//                        
+//
 //                        if let price = dict["last"] as? String, let prevDay = dict["open"] as? String {
-//                            
-//                            
+//
+//
 //                            usdPrice = Double(price)!
 //                            yesterdayPrice = Double(prevDay)!
-//                            
+//
 //                        }
-//                        
+//
 //                    })
-//                    
+//
 //                    _ = array.map({ dict -> Void in
 //                        if let symbol = dict["symbol"] as? String, let price = dict["last"] as? String, let prevDay = dict["open"] as? String {
-//                            
+//
 //                            if symbol.suffix(3) == "BTC" {
 //                                //                                print("\(symbol) - \(price) - \(change)")
-//                                
+//
 //                                let sym = symbol.dropLast(3).description
-//                                
-//                                
+//
+//
 //                                let tickerUSDPrice = Double(price)! * usdPrice
-//                                
+//
 //                                let changeBTC = (Double(price)! - Double(prevDay)!) / Double(prevDay)!
-//                                
+//
 //                                let priceBTC = Double(price)!
-//                                
-//                                
+//
+//
 //                                let assetYesterdayPice = yesterdayPrice * Double(price)! * Double(prevDay)!
-//                                
+//
 //                                //                                let tickerYesterdayPrice = yesterdayPrice * assetYesterdayPice
-//                                
+//
 //                                let changeUSD = (tickerUSDPrice - assetYesterdayPice) / assetYesterdayPice
-//                                
+//
 //                                //                                let changeBTC = NSDecimalNumber(string: change).dividing(by: NSDecimalNumber(value: 100))
-//                                
+//
 //                                let ticker = ["symbol" : sym,
 //                                              "priceBTC" : priceBTC.description,
 //                                              "changeBTC" : changeBTC.description,
 //                                              "priceUSDT" : tickerUSDPrice.description,
 //                                              "changeUSDT" : changeUSD.description]
-//                                
-//                                
+//
+//
 //                                //                                    Ticker(symbol: sym, priceUSDT: tickerUSDPrice.description, priceBTC: price, changeUSDT: changeUSD.description, changeBTC: changeBTC.stringValue)
 //                                tickers.append(ticker as [String : AnyObject])
-//                                
+//
 //                            }
 //                        }
 //                    })
-//                    
+//
 //                    //                    print(tickers)
-//                    
+//
 //                    completion(tickers)
-//                    
+//
 //                } catch {
 //                    print("Error: cannot create JSON from todo")
 //                    return
 //                }
-//                
-//                
+//
+//
 //            }
-//            
+//
 //            }.resume()
 //    }
     
